bdecc2f8e226f22c21cf8202606e7fd0ae72e80b
Tsilavina Arnaud <tsilavinaarnaud8@gmail.com>
2025-08-14 11:08:04 +0200

    feat: list all applications with search features

--- src/components/views/ApplicationsView.jsx
+++ src/components/views/ApplicationsView.jsx
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../Form/Input";
 import SimpleBtn from "../Button/SimpleBtn";
 import Table from "../Table/Table";
-import { useState, useEffect } from "react";
 import useFetch from "../../hooks/useFetch";
 import { Outlet } from "react-router-dom";
 
 function ApplicationsView() {
-  const tableTitle = ["PID", "name", "title", "CPU", "Actions"];
+  const tableTitle = ["PID", "title", "CPU", "Actions"];
   const [q, setQ] = useState("");
 
   const searchApps = (app) => {
@@ -29,7 +28,7 @@ function ApplicationsView() {
   if (data) {
     data
       .filter((proc) =>
-        proc.process_name.toLowerCase().includes(q.toLowerCase())
+        proc.window_title.toLowerCase().includes(q.toLowerCase())
       )
       .map((process, key) =>
         applications.push(
@@ -38,10 +37,8 @@ function ApplicationsView() {
             key={key}
           >
             <td className="py-4">{process.pid}</td>
-            <td className="py-4">{process.process_name}</td>
-            <td className="py-4">{process.process_name}</td>
-
-            <td className="py-4">{process.cpu} %</td>
+            <td className="py-4">{process.window_title}</td>
+            <td className="py-4">{process.cpu_percent} %</td>
             <td>
               <div className="flex justify-center gap-4">
                 <button className="py-2 px-4 text-white bg-green-600">Detail</button>
@@ -56,14 +53,8 @@ function ApplicationsView() {
   return (
     <>
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl">Applications</h1>
+        <h1 className="text-2xl">History</h1>
         <Input searchApps={searchApps} />
-        <div>
-          <SimpleBtn label="Show details" bg="bg-amber-400" />
-        </div>
-        <div>
-          <SimpleBtn label="Quit application" bg="bg-red-600" />
-        </div>
       </div>
       <div className="flex mt-8">
         {/* <div className="my-8"> */}

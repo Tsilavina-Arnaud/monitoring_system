c80608867e1e4d2c6c0c5cb6a7c70ebd693016db
Tsilavina Arnaud <tsilavinaarnaud8@gmail.com>
2025-09-06 07:53:23 +0200

    feat: Display overview page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import SideNav from "./components/SideNav/SideNav";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import HomeView from "./components/views/HomeView";
@@ -12,6 +12,17 @@ import { MdDashboard } from "react-icons/md";
 import { FaChartLine } from "react-icons/fa6";
 import { IoMdSpeedometer } from "react-icons/io";
 import { VscServerProcess } from "react-icons/vsc";
+import Box from "@mui/material/Box";
+import RadioGroup from "@mui/material/RadioGroup";
+import Radio from "@mui/material/Radio";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+import {
+  ThemeProvider,
+  createTheme,
+  useColorScheme,
+} from "@mui/material/styles";
 
 const router = createBrowserRouter([
   {
@@ -40,24 +51,24 @@ const router = createBrowserRouter([
 
 const links = [
   {
-    name: "Aperçu",
+    name: "Overview",
     link: "/",
-    icon: <IoMdSpeedometer className="text-2xl" />
+    icon: <IoMdSpeedometer className="text-2xl" />,
   },
   {
     name: "Applications",
     link: "/applications",
-    icon: <MdDashboard className="text-2xl" />
+    icon: <MdDashboard className="text-2xl" />,
   },
   {
-    name: "Historique",
+    name: "History",
     link: "/history",
-    icon: <FaChartLine className="text-2xl" />
+    icon: <FaChartLine className="text-2xl" />,
   },
   {
-    name: "Processus",
+    name: "Processes",
     link: "/process",
-    icon: <VscServerProcess className="text-2xl" />
+    icon: <VscServerProcess className="text-2xl" />,
   },
 ];
 
@@ -65,15 +76,75 @@ function App() {
   return <RouterProvider router={router} />;
 }
 
-function Root() {
-  const {data} = useFetch('/api/hostname')
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 
+function Root() {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <MyApp />
+      </ThemeProvider>
+      ;
+    </>
+  );
+}
+
+function MyApp() {
+  const { data } = useFetch("/api/hostname");
+  const { mode, setMode } = useColorScheme();
+  if (!mode) {
+    return null;
+  }
   return (
     <>
       <div className="flex items-baseline">
         <SideNav links={links} />
         <div className="w-4/5 relative left-1/5 container px-4 my-4">
-          <div className="flex items-center justify-end">
+          <div className="flex items-center justify-between">
+            <div>
+              <Box
+                sx={{
+                  display: "flex",
+                  width: "100%",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  color: "text.primary",
+                  borderRadius: 1,
+                  p: 3,
+                  minHeight: "56px",
+                }}
+              >
+                <FormControl>
+                  <RadioGroup
+                    aria-labelledby="demo-theme-toggle"
+                    name="theme-toggle"
+                    row
+                    value={mode}
+                    onChange={(event) => setMode(event.target.value)}
+                  >
+                    <FormControlLabel
+                      value="system"
+                      control={<Radio />}
+                      label="System"
+                    />
+                    <FormControlLabel
+                      value="light"
+                      control={<Radio />}
+                      label="Light"
+                    />
+                    <FormControlLabel
+                      value="dark"
+                      control={<Radio />}
+                      label="Dark"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </Box>
+            </div>
             <h1 className="text-2xl mx-8">{data.hostname}</h1>
           </div>
           <div className="mt-8">
--- src/components/views/HomeView.jsx
+++ src/components/views/HomeView.jsx
@@ -1,13 +1,198 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Stack from "@mui/material/Stack";
+import { Gauge } from "@mui/x-charts/Gauge";
+import {
+  GaugeContainer,
+  GaugeValueArc,
+  GaugeReferenceArc,
+  useGaugeState,
+} from "@mui/x-charts/Gauge";
+import useFetch from "../../hooks/useFetch";
+import ApplicationsView from "./ApplicationsView";
+
+function GaugePointer() {
+  const { valueAngle, outerRadius, cx, cy } = useGaugeState();
+
+  if (valueAngle === null) {
+    return null;
+  }
+
+  const target = {
+    x: cx + outerRadius * Math.sin(valueAngle),
+    y: cy - outerRadius * Math.cos(valueAngle),
+  };
+  return (
+    <g>
+      <circle cx={cx} cy={cy} r={5} fill="red" />
+      <path
+        d={`M ${cx} ${cy} L ${target.x} ${target.y}`}
+        stroke="red"
+        strokeWidth={3}
+      />
+    </g>
+  );
+}
 
 function HomeView() {
-    
+  const { data, refetch } = useFetch("/api/overview");
 
-  return (
-    <div>
-      <h1 className="text-2xl">Home</h1>
+  useEffect(() => {
+    setInterval(() => {
+      refetch();
+    }, 1000);
+  }, [refetch]);
+
+  let DDrive = data["D:"];
+  let CDrive = data["C:"];
+  let memory = data["memory"];
+
+  const view = (
+    <div className="mt-5">
+      <div className="grid grid-cols-2 gap-5">
+        <div className=" col-span-1 border-2 border-gray-600 rounded-2xl py-4">
+          <div className="flex justify-center items-center">
+            <div>
+              <div>
+                <h3 className="text-center text-2xl">Disk C:</h3>
+                <div className="flex justify-center">
+                  <GaugeContainer
+                    width={200}
+                    height={200}
+                    startAngle={-110}
+                    endAngle={110}
+                    value={(CDrive?.used * 100) / CDrive?.total}
+                  >
+                    <GaugeReferenceArc />
+                    <GaugeValueArc />
+                    <GaugePointer />
+                  </GaugeContainer>
+                </div>
+              </div>
+              <div className="my-3">
+                <div className="flex justify-between items-center">
+                  <div>
+                    <Gauge width={100} height={100} value={CDrive?.used} />
+                    <p className="text-center text-sm text-gray-400">Used</p>
+                  </div>
+                  <div>
+                    <Gauge width={100} height={100} value={CDrive?.free} />
+                    <p className="text-center text-sm text-gray-400">Free</p>
+                  </div>
+                  <div>
+                    <Gauge width={100} height={100} value={CDrive?.total} />
+                    <p className="text-center text-sm text-gray-400">Total</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" col-span-1 border-2 border-gray-600 rounded-2xl py-4">
+          <div className="flex justify-center items-center">
+            <div>
+              <div>
+                <h3 className="text-center text-2xl">Disk D:</h3>
+                <div className="flex justify-center">
+                  <GaugeContainer
+                    width={200}
+                    height={200}
+                    startAngle={-110}
+                    endAngle={110}
+                    value={(DDrive?.used * 100) / DDrive?.total}
+                  >
+                    <GaugeReferenceArc />
+                    <GaugeValueArc />
+                    <GaugePointer />
+                  </GaugeContainer>
+                </div>
+              </div>
+              <div className="my-3">
+                <div className="flex justify-between items-center">
+                  <div>
+                    <Gauge width={100} height={100} value={DDrive?.used} />
+                    <p className="text-center text-sm text-gray-400">Used</p>
+                  </div>
+                  <div>
+                    <Gauge width={100} height={100} value={DDrive?.free} />
+                    <p className="text-center text-sm text-gray-400">Free</p>
+                  </div>
+                  <div>
+                    <Gauge width={100} height={100} value={DDrive?.total} />
+                    <p className="text-center text-sm text-gray-400">Total</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" col-span-1 border-2 border-gray-600 rounded-2xl py-4">
+          <div className="flex justify-center items-center">
+            <div>
+              <h3 className="text-center text-2xl">CPU</h3>
+              <GaugeContainer
+                width={200}
+                height={200}
+                startAngle={-110}
+                endAngle={110}
+                value={data.cpu}
+              >
+                <GaugeReferenceArc />
+                <GaugeValueArc />
+                <GaugePointer />
+              </GaugeContainer>
+            </div>
+          </div>
+        </div>
+        <div className=" col-span-1 border-2 border-gray-600 rounded-2xl py-4">
+          <div className="flex justify-center items-center">
+            <div>
+              <div>
+                <h3 className="text-center text-2xl">Memory</h3>
+                <div className="flex justify-center">
+                  <GaugeContainer
+                    width={200}
+                    height={200}
+                    startAngle={-110}
+                    endAngle={110}
+                    value={(memory?.used * 100) / memory?.total}
+                  >
+                    <GaugeReferenceArc />
+                    <GaugeValueArc />
+                    <GaugePointer />
+                  </GaugeContainer>
+                </div>
+              </div>
+              <div className="my-3">
+                <div className="flex justify-between items-center">
+                  <div>
+                    <Gauge
+                      width={100}
+                      height={100}
+                      value={(memory?.used * 100) / memory?.total}
+                    />
+                    <p className="text-center text-sm text-gray-400">Used</p>
+                  </div>
+                  <div>
+                    <Gauge
+                      width={100}
+                      height={100}
+                      value={(memory?.free * 100) / memory?.total}
+                    />
+                    <p className="text-center text-sm text-gray-400">Free</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-4">
+        <ApplicationsView />
+      </div>
     </div>
   );
+
+  return <div>{data ? view : "Loading"}</div>;
 }
 
 export default HomeView;
